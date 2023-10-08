48a6dde09d9186935c97edbb89c837c52744af97
wizzyszn <wizzywizpad@gmail.com>
2023-10-08 07:37:39 +0100

    i just learnt the intersection Observer API from scratch

--- nav-change-finished/js/observers.js
+++ nav-change-finished/js/observers.js
@@ -2,21 +2,20 @@ const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home-intro");
 
 const sectionOneOptions = {
-  rootMargin: "-200px 0px 0px 0px"
+  rootMargin : "-200px 0px 0px 0px"
 };
-
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("nav-scrolled");
-    } else {
-      header.classList.remove("nav-scrolled");
+const callback = (entries,sectionOneObserver) =>{
+  entries.forEach((entry)=>{
+    console.log(entry.isIntersecting)
+    if(!entry.isIntersecting){
+      header.classList.add('nav-scrolled')
+    
+    }else{
+      header.classList.remove('nav-scrolled')
     }
-  });
-},
-sectionOneOptions);
 
-sectionOneObserver.observe(sectionOne);
+  })
+}
+const sectionOneObserver = new IntersectionObserver(callback, sectionOneOptions)
+
+sectionOneObserver.observe(sectionOne)
